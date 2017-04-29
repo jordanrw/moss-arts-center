8ca5e5e7b46672fe958f0b1d9af1df610ad5d37b
Andrew Subowo <andrew.subowo@gmail.com>
2017-04-28 23:27:43 -0400

    Finalize implementation with Firebase Console

---
+++ app/src/main/java/com/mossartscenter/mossartscenterpatrons/FirebaseNotificationService.java
@@ -0,0 +1,85 @@
+package com.mossartscenter.mossartscenterpatrons;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+/**
+ * Created by sudowoodo on 4/28/17.
+ */
+
+public class FirebaseNotificationService extends FirebaseMessagingService {
+
+    private static final String TAG = "MACFirebaseMessagingService";
+
+    /**
+     * Called when a message is pushed from the Firebase console.
+     *
+     * @param remoteMessage The remote message from firebase
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Map<String, String> data = remoteMessage.getData();
+        String icon = data.get("icon");
+
+
+        String title = data.get("text");//remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ticket)
+
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        //displayNotification(body, title);
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
+
+    }
+
+    private void displayNotification(String messageBody, String title) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ticket)
+                .setContentTitle(title)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
+
